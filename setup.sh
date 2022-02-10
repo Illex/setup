6e9ae55a4ad4dd95077e4cb2e5dc49c1d2880f63
Illex <kylamoureux@gmail.com>
2022-02-10 12:32:21 -0700

    Update setup.sh
    fixed syntax error

--- setup.sh
+++ setup.sh
@@ -87,7 +87,7 @@ os.system("sudo mkdir ../.config/i3")
 os.system("sudo cp config ../.config/i3/config")
 
 #copy i3 bar conf to right location
-os.system("sudo cp i3status.conf ../etc
+os.system("sudo cp i3status.conf ../etc")
 
 #copy the airbladder compton config
 os.system("sudo cp compton.conf ../.config")
